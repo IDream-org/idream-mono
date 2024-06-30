706bf2c98e76731be45c42e06f9375e25df5f7e0
Matheus Silva <msilvaqs@outlook.com>
2024-06-30 10:49:16 -0400

    Removing commented out code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,4 @@
-// import { NextRequest, NextResponse } from "next/server";
-
 export { default } from "next-auth/middleware";
-
-// export function middleware(request: NextRequest) {
-//   if (!request.cookies.has("next-auth.session-token")) {
-//     return NextResponse.redirect(new URL("/", request.url));
-//   }
-// }
 
 export const config = {
   matcher: ["/dashboard", "/profile", "/collections/:slug*"],
